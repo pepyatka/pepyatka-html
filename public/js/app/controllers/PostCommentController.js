c7155e2afa9df541f4d8b8f52cd560bba251d404
epicmonkey <mail@epicmonkey.org>
2015-03-22 00:04:21 +0300

    Implements edit comment.

--- public/js/app/controllers/PostCommentController.js
+++ public/js/app/controllers/PostCommentController.js
@@ -2,5 +2,18 @@ define(["config",
         "app/app",
         "ember"], function(config, App, Ember) {
   App.PostCommentController = Ember.Controller.extend({
+    body: Ember.computed.oneWay('model.body'),
+
+    actions: {
+      update: function() {
+        var comment = this.get('model')
+        var body = this.get('body', '')
+
+        comment.set('body', body)
+        comment.save()
+          .then(function(newComment) {
+          }.bind(this))
+      }
+    }
   })
 })
