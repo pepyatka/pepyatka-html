6cd1b9234fd0faa58e7be86144fe0596344d1c50
epicmonkey <mail@epicmonkey.org>
2015-03-25 00:52:42 +0300

    Synchronizes river of news page with actions and template.

--- public/js/app/controllers/TimelineHomeController.js
+++ public/js/app/controllers/TimelineHomeController.js
@@ -1,5 +1,21 @@
 define(["app/app",
         "ember"], function(App, Ember) {
   App.TimelineHomeController = Ember.Controller.extend({
+    postSortProperties: ['createdAt:desc'],
+    posts: Ember.computed.sort('model.posts', 'postSortProperties'),
+
+    actions: {
+      create: function() {
+        var post = this.store.createRecord('post', {
+          body: this.get('body')
+        })
+
+        this.set('body', '')
+        post.save()
+          .then(function(post) {
+            this.get('content.posts').pushObject(post)
+          }.bind(this))
+      }
+    }
   })
 })
