c7913846ae3c759b2c957badb9fd61a87f188bc5
Alex Olshansky <clbn@spotify.com>
2015-03-30 03:15:47 +0200

    Attachments: model, list of existing attachments
    * There is no attachment uploading yet
    * Shows either image or link to the file: no fancy player for audio

--- public/js/app/models/Attachment.js
+++ public/js/app/models/Attachment.js
@@ -3,7 +3,12 @@ define(["app/app"], function(App) {
 
   App.Attachment = DS.Model.extend({
     identifier: DS.attr('string'),
+    filename: DS.attr('string'),
+    isImage: DS.attr('boolean'),
+    createdAt: DS.attr('number'),
+    updatedAt: DS.attr('number'),
 
+    createdBy: DS.belongsTo('user'),
     post: DS.belongsTo('post')
   })
 })
