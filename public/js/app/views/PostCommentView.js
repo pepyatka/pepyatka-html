89fc7fcb2c6a9671fa49f06eb004aea7ecddb5f4
epicmonkey <mail@epicmonkey.org>
2015-03-30 12:56:53 +0300

    Adds an action to show/hide edit comment form.

--- public/js/app/views/PostCommentView.js
+++ public/js/app/views/PostCommentView.js
@@ -1,7 +1,17 @@
 define(["app/app",
         "text!templates/postCommentTemplate.handlebars"], function(App, tpl) {
+   "use strict";
+
   App.PostCommentView = Ember.View.extend({
     templateName: 'post-comment',
-    template: Ember.Handlebars.compile(tpl)
+    template: Ember.Handlebars.compile(tpl),
+
+    isEdit: false,
+
+    actions: {
+      toggleEditability: function() {
+        this.toggleProperty('isEdit')
+      }
+    }
   })
 })
