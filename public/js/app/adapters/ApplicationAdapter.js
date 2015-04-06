e6344ed49c8b5a7f460cc0013edc51297ed3beb4
epicmonkey <mail@epicmonkey.org>
2015-04-07 01:33:52 +0300

    Replaces hardcoded localhost in ApplicationAdapter to one from config.

--- public/js/app/adapters/ApplicationAdapter.js
+++ public/js/app/adapters/ApplicationAdapter.js
@@ -1,11 +1,12 @@
-define(["app/app",
-        "ember"], function(App, Ember) {
+define(["config",
+        "app/app",
+        "ember"], function(config, App, Ember) {
   // TODO: this document
   // http://emberjs.com/api/data/classes/DS.RESTAdapter.html#toc_headers-customization
   // describes how we can set headers in RESTAdapter and so make
   // App.Session slimer
   App.ApplicationAdapter = DS.RESTAdapter.extend({
-    host: 'http://localhost:3000',
+    host: config.host,
     namespace: 'v1'
   })
 })
