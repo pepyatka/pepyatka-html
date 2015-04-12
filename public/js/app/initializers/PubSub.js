bac2eafe14ff44a2bd98a7664efe3d22e9848204
epicmonkey <mail@epicmonkey.org>
2015-04-13 01:09:49 +0300

    Changes pubsub events to namespaces.

--- public/js/app/initializers/PubSub.js
+++ public/js/app/initializers/PubSub.js
@@ -20,18 +20,18 @@ define(["config",
 
           this.set('socket', io.connect(config.host + '/'))
 
-          this.get('socket').on('newPost', this.newPost.bind(this))
-          this.get('socket').on('updatePost', this.updatePost.bind(this))
-          this.get('socket').on('destroyPost', this.destroyPost.bind(this))
-          this.get('socket').on('hidePost', this.hidePost.bind(this))
-          this.get('socket').on('unhidePost', this.unhidePost.bind(this))
+          this.get('socket').on('post:new', this.newPost.bind(this))
+          this.get('socket').on('post:update', this.updatePost.bind(this))
+          this.get('socket').on('post:destroy', this.destroyPost.bind(this))
+          this.get('socket').on('post:hide', this.hidePost.bind(this))
+          this.get('socket').on('post:unhide', this.unhidePost.bind(this))
 
-          this.get('socket').on('newComment', this.newComment.bind(this))
-          this.get('socket').on('updateComment', this.updateComment.bind(this))
-          this.get('socket').on('destroyComment', this.destroyComment.bind(this))
+          this.get('socket').on('comment:new', this.newComment.bind(this))
+          this.get('socket').on('comment:update', this.updateComment.bind(this))
+          this.get('socket').on('comment:destroy', this.destroyComment.bind(this))
 
-          this.get('socket').on('newLike', this.newLike.bind(this))
-          this.get('socket').on('removeLike', this.removeLike.bind(this))
+          this.get('socket').on('like:new', this.newLike.bind(this))
+          this.get('socket').on('like:remove', this.removeLike.bind(this))
 
           this.get('socket').on('disconnect', this.disconnect.bind(this))
         },
