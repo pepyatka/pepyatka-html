3b4dce263b01c519614256e6aa34ea11afbad243
Alex Kapranoff <alex@kapranoff.ru>
2015-04-19 20:07:24 +0000

    Make "profile-links" aware of current user
    Fix #132 and #178.

--- public/js/app/views/ProfileLinksView.js
+++ public/js/app/views/ProfileLinksView.js
@@ -4,6 +4,15 @@ define(["app/app",
 
   App.ProfileLinksView = Ember.View.extend({
     templateName: 'profileLinks',
-    template: Ember.Handlebars.compile(tpl)
+    template: Ember.Handlebars.compile(tpl),
+
+    isOwner: function() {
+      if (!this.get('controller.session.signedIn'))
+        return false
+
+      var userId = this.get('controller.session.currentUser.id')
+      var ownerId = this.get('controller.model.user.id')
+      return userId == ownerId
+    }.property('controller.model.user.id', 'controller.session.currentUser.id')
   })
 })
