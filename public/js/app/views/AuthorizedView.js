a30f596569709518c90e2b53c9e0b35cd1f81524
epicmonkey <mail@epicmonkey.org>
2015-04-23 15:17:24 +0300

    Fixes #242

--- public/js/app/views/AuthorizedView.js
+++ public/js/app/views/AuthorizedView.js
@@ -1,8 +1,14 @@
 define(["app/app",
-        "text!layouts/authorizedLayout.handlebars"], function(App, tpl) {
+        "text!layouts/authorizedLayout.handlebars",
+        "text!layouts/unauthorizedLayout.handlebars"],
+       function(App, authTemplate, unauthTemplate) {
   "use strict";
 
   App.AuthorizedView = Ember.View.extend({
-    layout: Ember.Handlebars.compile(tpl)
+    layout: function() {
+      var user = this.get('controller.session.currentUser')
+      var template = user ? authTemplate : unauthTemplate
+      return Ember.Handlebars.compile(template)
+    }.property('controller.session.currentUser.id')
   })
 })
