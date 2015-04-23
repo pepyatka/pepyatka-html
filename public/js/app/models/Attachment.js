657275201a37092fe37933949cc6028cf38269c0
epicmonkey <mail@epicmonkey.org>
2015-04-23 20:14:17 +0300

    Restores FormData attribute in Attachment model.

--- public/js/app/models/Attachment.js
+++ public/js/app/models/Attachment.js
@@ -2,6 +2,7 @@ define(["app/app"], function(App) {
   "use strict";
 
   App.Attachment = DS.Model.extend({
+    file: DS.attr('file'), // FormData File object
     url: DS.attr('string'),
     thumbnailUrl: DS.attr('string'),
     fileName: DS.attr('string'),
