7366c70f8d4a9db448347bc8ff9da40c4b26eea7
epicmonkey <mail@epicmonkey.org>
2015-04-23 18:42:32 +0300

    Fixes #186 and #24.

--- public/js/app/views/PostLikesView.js
+++ public/js/app/views/PostLikesView.js
@@ -4,6 +4,30 @@ define(["app/app",
 
   App.PostLikesView = Ember.View.extend({
     templateName: 'post-likes',
-    template: Ember.Handlebars.compile(tpl)
+    template: Ember.Handlebars.compile(tpl),
+
+    hidden: true,
+
+    isHidden: function() {
+      return this.get('hidden') && this.get('likes.length') > 4
+    }.property('likes.length', 'hidden'),
+
+    shownLikes: function() {
+      return this.get('likes').filter(function(item, index) {
+        if (index < 3) { return true }
+      })
+    }.property('likes'),
+
+    hiddenLikes: function() {
+      return this.get('likes').filter(function(item, index) {
+        if (index >= 3) { return true }
+      })
+    }.property('likes'),
+
+    actions: {
+      toggleHidden: function() {
+        this.toggleProperty('hidden')
+      }
+    }
   })
 })
