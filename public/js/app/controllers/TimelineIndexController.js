749b5e18ec5edda521126cbc167a08cedd2bd3d3
epicmonkey <mail@epicmonkey.org>
2015-04-29 13:49:26 +0300

    Fixes a defect when unauth user cannot open user profile page.

--- public/js/app/controllers/TimelineIndexController.js
+++ public/js/app/controllers/TimelineIndexController.js
@@ -9,7 +9,10 @@ define(["config",
     selectFeedsOnCreate: false,
 
     isSubscribed: function() {
-      return this.get('session.currentUser.subscriptions').isAny('id', this.get('model.id'))
+      var currentUser = this.get('session.currentUser')
+      if (!currentUser) { return false }
+
+      return currentUser.get('subscriptions').isAny('id', this.get('model.id'))
     }.property('session.currentUser.id', 'session.currentUser.subscriptions.@each', 'model.id'),
 
     actions: {
