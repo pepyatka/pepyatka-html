81f17d0600153ac598d410dbc01e389b2d57932d
epicmonkey <mail@epicmonkey.org>
2015-06-21 20:49:19 -0400

    Hides ban user link on Likes/Comments pages. Fixes #489.

--- public/js/app/views/ProfileLinksView.js
+++ public/js/app/views/ProfileLinksView.js
@@ -13,6 +13,10 @@ define(["app/app",
       var userId = this.get('controller.session.currentUser.id')
       var ownerId = this.get('controller.model.user.id')
       return userId == ownerId
-    }.property('controller.model.user.id', 'controller.session.currentUser.id')
+    }.property('controller.model.user.id', 'controller.session.currentUser.id'),
+
+    isTimelineIndex: function() {
+      return this.get('parentView.templateName') === 'timeline/index'
+    }.property('parentView.templateName')
   })
 })
