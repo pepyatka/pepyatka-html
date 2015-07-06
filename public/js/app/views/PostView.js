a43c56c5ecf40d011822c04f1e672ffcb054ce62
Alex Olshansky <i@creagenics.com>
2015-07-06 14:43:12 +0200

    Add attachment expand/collapse (single post page)
    Fixes #525.

--- public/js/app/views/PostView.js
+++ public/js/app/views/PostView.js
@@ -16,6 +16,46 @@ define(["app/app",
       return userId == ownerId
     }.property('controller.content.createdBy'),
 
+    hideSurplusAttachments: function() {
+      var that = this
+      Ember.run.scheduleOnce('afterRender', this, function() {
+        var container = this.$().find('.attachments')
+        var nodes = container.find('.attachment')
+
+        if (nodes.size() === 0) {
+          return
+        }
+
+        container.loadedAttachments = 0
+
+        container.find('img').on('load', function() {
+          // This event fires for every image, but we need to process each post only once
+          // (delegation isn't supported for 'load' event)
+          container.loadedAttachments++
+          if (container.loadedAttachments === nodes.size()) {
+
+            // Find how many attachments are on the first row
+            var quantity = 1
+            var firstRowOffset = nodes[0].offsetTop + nodes[0].offsetHeight / 2
+            var anotherOffset = 0
+            for (var i = 1; i < nodes.size(); i++) {
+              anotherOffset = nodes[i].offsetTop + nodes[i].offsetHeight / 2
+              if (anotherOffset > firstRowOffset) {
+                break
+              }
+              quantity++
+            }
+
+            // Hide surplus
+            if (nodes.size() > quantity) {
+              that.set('controller.hasSurplusAttachments', true)
+              nodes.slice(quantity).addClass('surplus')
+            }
+          }
+        })
+      })
+    }.on('didInsertElement'),
+
     actions: {
       focusCommentForm: function() {
         this.$('.edit-comment-area').focus()
