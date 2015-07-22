03bb94b1cbc66ed4121dfd6cc10f49740b8633c3
Berkus Decker <berkus@users.noreply.github.com>
2015-07-22 12:47:47 +0300

    Always return fixed-point representation for size.
    Avoids problems from #583 entirely.

--- public/js/app/models/Attachment.js
+++ public/js/app/models/Attachment.js
@@ -34,10 +34,9 @@ define(["app/app"], function(App) {
 
       if (bytes == 0) return '0 Byte'
       var k = 1000
-      var dm = decimals + 1 || 3
       var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
       var i = Math.floor(Math.log(bytes) / Math.log(k))
-      return (bytes / Math.pow(k, i)).toPrecision(dm) + ' ' + sizes[i]
+      return (bytes / Math.pow(k, i)).toFixed(decimals) + ' ' + sizes[i]
     }.property('fileSize')
   })
 })
