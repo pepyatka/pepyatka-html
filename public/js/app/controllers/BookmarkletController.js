55b8a95e6b396ce5d77dac376f67d3c954591ac9
Alex Olshansky <i@creagenics.com>
2015-09-18 18:27:39 +0200

    [bookmarklet] Add throbber and disabled button

--- public/js/app/controllers/BookmarkletController.js
+++ public/js/app/controllers/BookmarkletController.js
@@ -7,26 +7,31 @@ define(["config",
   App.BookmarkletController = App.ApplicationController.extend({
     queryParams: ['title', 'comment'],
 
+    isSending: false,
     isSent: false,
     postData: null,
 
     actions: {
       send: function() {
+        this.set('isSending', true)
+
         var data = {
           title: Ember.$('#bookmarklet-title').val(),
           image: Ember.$('#bookmarklet-image-url').val(),
           comment: Ember.$('#bookmarklet-comment').val()
         }
+
         Ember.$.ajax({
           url: config.host + '/v1/bookmarklet',
           data: data,
           type: 'post',
           context: this
         })
-          .then(function(response) {
-            this.set('postData', response)
-            this.set('isSent', true)
-          })
+        .then(function(response) {
+          this.set('postData', response)
+          this.set('isSent', true)
+          this.set('isSending', false)
+        })
       }
     }
   })
